e403d2816afe0aa07ebd91276f0e8fa5d9a3abc0
Wenjing Li <289639308@qq.com>
2023-10-20 04:37:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,18 +11,19 @@ author wenjing
 import argparse
 from dataset import DataSet
 import torch
-from utils import get_test_dataloader, get_torch_network, modify_output
+from utils import get_test_dataloader, get_torch_network, modify_output, get_test_split, get_mean_std
 import time
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
+    parser.add_argument('-dataset', type=str, required=True, help='the name of the dataset')
     parser.add_argument('-gpu', type=bool, default=True, help='use gpu or not')
     parser.add_argument('-pretrain', type=bool, default=False, help='use pretrain model or not')
-    parser.add_argument('-b', type=int, default=16, help='batch size for dataloader')
+    parser.add_argument('-b', type=int, default=256, help='batch size for dataloader')
     parser.add_argument('-num_class', type=int, default=22, help='the number of the classes')
-    parser.add_argument('-model_root', type=int, default=22, help='the weights file you want to test')
+    parser.add_argument('-model_root', type=str, required = True, help='the weights file you want to test')
     args = parser.parse_args()
     net = get_torch_network(args)
 
@@ -32,13 +33,14 @@ if __name__ == '__main__':
     net = net.cuda()
  
     checkpoint = torch.load(args.model_root)
+    net.load_state_dict(checkpoint)
 
     net.eval()
 
-
+    test_root, test_split = get_test_split(args.dataset)
+    mean, std = get_mean_std(args.dataset)
 # prepare test data
-    val_datasets = DataSet('/home/liwenjing/Backbones/data/PIC/day/angle2_rgb',
-                           '/home/liwenjing/Backbones/data/PIC/day-xiandai/cam2/test.txt', flag='val')
+    val_datasets = DataSet(test_root, test_split, mean, std, flag='val')
     test_loader = get_test_dataloader(
         dataset = val_datasets,
         num_workers=4,
@@ -51,7 +53,6 @@ if __name__ == '__main__':
     correct_1 = 0.0
     correct_5 = 0.0
     total = 0
-
     with torch.no_grad():
         for n_iter, (image, label) in enumerate(test_loader):
             print("iteration: {}\ttotal {} iterations".format(n_iter + 1, len(test_loader)))
@@ -64,7 +65,7 @@ if __name__ == '__main__':
             output = net(image)
             time1 = time.time()
             timed = time1 - time0
-            print('time',timed)
+            print('time', timed)
 
             _, pred = output.topk(5, 1, largest=True, sorted=True)
 
@@ -79,6 +80,7 @@ if __name__ == '__main__':
 
 
     print()
-    print("Top 1 err: ", 1 - correct_1 / len(test_loader.dataset))
-    print("Top 5 err: ", 1 - correct_5 / len(test_loader.dataset))
+    print("Top 1 acc: ", correct_1 / len(test_loader.dataset))
+    print("Top 5 acc: ", correct_5 / len(test_loader.dataset))
     print("Parameter numbers: {}".format(sum(p.numel() for p in net.parameters())))
+
